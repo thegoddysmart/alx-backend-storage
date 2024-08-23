bf3b974931295606ce351da9d47338c80d716711
Godfred <goddysmart2468@gmail.com>
2024-08-23 05:10:27 +0000

    In and not out

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,9 @@
+-- This script generates a ranking of countries
+-- Ranks country origins of bands
+-- Ordered by the number of (non-unique) fans
+-- Column names must be: origin and nb_fans
+
+SELECT origin, SUM(fans) AS nb_fans
+FROM metal_bands 
+GROUP BY origin
+ORDER BY nb_fans DESC;
