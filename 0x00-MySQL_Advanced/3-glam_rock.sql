8ddbae4ffc60b46a0bdbbc33a17b99589acdf696
Godfred <goddysmart2468@gmail.com>
2024-08-23 05:12:22 +0000

    Old school band

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,7 @@
+-- List all bands with Glam rock as their style,
+-- ranked by their longevity
+
+SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan
+    FROM metal_bands
+    WHERE style LIKE '%Glam rock%'
+    ORDER BY lifespan DESC;
